11f53a7c9325ffd515d2b4def9e0e8e9792c48a7
EJ <100031970+EJARNADO11@users.noreply.github.com>
2023-03-08 20:22:28 +0800

    Add files via upload

---
+++ System/inventory-system/receive-module/index.php
@@ -0,0 +1,30 @@
+<div id="third-submenu">
+    <a href="index.php?page=receive"><i class="fa-sharp fa-regular fa-pen-to-square"></i>Receiving List</a> | 
+    <a href="index.php?page=receive&action=create"><i class="fa-regular fa-square-plus"></i>New Transaction</a> | 
+
+    Search <input type="text"/>
+</div>
+<div id="subcontent">
+    <?php
+      switch($action){
+                case 'create':
+                    require_once 'receive-module/create-transaction.php';
+                break; 
+                case 'receive':
+                    require_once 'receive-module/receive-products.php';
+                break; 
+                case 'profile':
+                    require_once 'receive-module/view-product.php';
+                break;
+                case 'types':
+                    require_once 'receive-module/list-product-types.php';
+                break;
+                case 'upload':
+                    require_once 'receive-module/imageupload.php';
+                break;
+                default:
+                    require_once 'receive-module/main.php';
+                break; 
+            }
+    ?>
+  </div>
